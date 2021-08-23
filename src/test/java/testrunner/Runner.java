d3fb4cc8e62204a2b54d1675b1f179b965e43da4
Deepak Dude <deepak_vtest@appveen.com>
2021-08-23 11:35:20 +0530

    Update code

--- src/test/java/testrunner/Runner.java
+++ src/test/java/testrunner/Runner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="./testcases/string_Text.feature",
+@CucumberOptions(features="./testcases/number_Currency.feature",
 				dryRun=false,
 				glue="stepdefinitions",
 				monochrome=true,
